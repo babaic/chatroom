683dc6e96752cb1bd0d2cfa165c87fefc142abaf
haris babaic <haris.babaic@outlook.com>
2020-07-13 21:46:24 +0200

    EF_context

---
+++ Chatroom.API/EF/MyContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Chatroom.API.EF
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> x) : base(x)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.UseSqlServer("Server=.;Database=probaaaa;Trusted_Connection=True;MultipleActiveResultSets=true;User ID=sa;Password=test");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+        }
+
+
+        //public DbSet<User> User { get; set; }
+
+
+    }
+}
